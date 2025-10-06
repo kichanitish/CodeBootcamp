09dabd228b9b0c7ce0cc56f750fdc750f204bdfe
Nitish KS <nitishks@Mac-349.lan>
2025-10-06 19:33:19 -0400

    Week1 problems - List manipulation

---
+++ week1/SortColors.py
@@ -0,0 +1,52 @@
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+ 
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        m = 0
+
+        while m <= r:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l+=1
+                m+=1
+
+            elif nums[m] == 1:
+                m+=1
+
+            #nums[m] ==2
+            else:
+                nums[m], nums[r] = nums[r], nums[m]
+                r-=1
+
